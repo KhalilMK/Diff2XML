67a18f7888aca41e3a002e6ebc2e039a7546cd0e
Khalil <mohamed.khalil@ags-it.com>
2023-02-22 09:44:44 +0100

    - Minor updates

--- src/main/java/com/khalilmohamed/diff2xml/service/Diff2XMLService.java
+++ src/main/java/com/khalilmohamed/diff2xml/service/Diff2XMLService.java
@@ -3,10 +3,9 @@ package com.khalilmohamed.diff2xml.service;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPathExpressionException;
 import java.io.IOException;
 
 public interface Diff2XMLService {
-    String getDiffXML() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException, TransformerException;
+    String getDiffXML() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException;
 }
